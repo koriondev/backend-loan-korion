fbbc60ddf6ba9446598b2c3e850b9730386ff4f4
Ubuntu <ubuntu@ip-172-31-43-99.us-east-2.compute.internal>
2025-12-02 13:26:16 +0000

    add auth middleware

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const notificationController = require('../controllers/notificationController');
-const auth = require('../middleware/auth');
+const auth = require('../middleware/authMiddleware');
 
 router.get('/', auth, notificationController.getNotifications);
 router.put('/:id/read', auth, notificationController.markAsRead);
